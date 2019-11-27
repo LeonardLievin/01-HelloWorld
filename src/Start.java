e5a22f8d28f363fc2f6a281878862f0c1e052a6d
Leonard Lievin <lievin.leonard@gmail.com>
2019-11-27 17:40:01 -0500

    fonction fibonacci

--- src/Start.java
+++ src/Start.java
@@ -2,14 +2,33 @@
 public class Start {
 
 	/*
-	 * commentaire
+	 * Algorithme de rafraichissement de connaissances en langue Java
 	 */
 	
 	
 	public static void main(String[] args) {
 		
-		
+		String fibo = fibonacci(20);
+		System.out.println(fibo);
 	
+	}
+	
+	public static String fibonacci(int etape) {
+				
+		int premier = 0;
+		int deuxieme = 1;
+		
+		String retour = premier + ", "+ deuxieme;
+		
+		for (int i = 0; i < etape; i++) {
+			int nouveau = premier + deuxieme ;
+			retour += ", "+nouveau ;
+			premier = deuxieme ; 
+			deuxieme = nouveau;
+		}
+		
+		return retour;
+		
 	}
 
 }
